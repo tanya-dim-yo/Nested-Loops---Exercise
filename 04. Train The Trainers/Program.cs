11d417cebdd73d550c76ff06090ab1eb1a2ca045
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-05-27 05:08:39 +0300

    Add files via upload

---
+++ 04. Train The Trainers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TrainTheTrainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int juryNumber = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int presentationCount = 0;
+            double averagePresentationGrade = 0;
+            string presentationName;
+
+            while (input != "Finish")
+            {
+                presentationName = input;
+                double presentationTotalGrade = 0;
+
+                for (int i = 1; i <= juryNumber; i++)
+                {
+                    presentationTotalGrade += double.Parse(Console.ReadLine());
+                }
+
+                presentationTotalGrade = presentationTotalGrade / juryNumber;
+                averagePresentationGrade += presentationTotalGrade;
+
+                Console.WriteLine($"{presentationName} - {presentationTotalGrade:F2}.");
+                presentationCount++;
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Student's final assessment is {averagePresentationGrade / presentationCount:F2}.");
+        }
+    }
+}
