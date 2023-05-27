c0c02c8a080f7e664ffcd8983cbb6bc3ef1bd0fb
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-05-27 05:07:11 +0300

    Add files via upload

---
+++ 02. Equal Sums Even Odd Position/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace EqualSumsEvenOddPositions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            for (int i = number1; i <= number2; i++)
+            {
+                string currentNum = i.ToString();
+                int evenSum = 0;
+                int oddSum = 0;
+
+                for (int j = 0; j < currentNum.Length; j++)
+                {
+                    int currentDigit = int.Parse(currentNum[j].ToString());
+
+                    if (j % 2 == 0)
+                    {
+                        evenSum += currentDigit;
+                    }
+                    else
+                    {
+                        oddSum += currentDigit;
+                    }
+                }
+
+                if (evenSum == oddSum)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
